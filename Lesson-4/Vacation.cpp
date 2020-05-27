7c42441c869f6d0564ca310cbf0760a5d70a850b
Daniel <48064871+Liu-Zhi-Yu@users.noreply.github.com>
2020-05-26 20:53:00 -0700

    Create Vacation.cpp

---
+++ Lesson-4/Vacation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int degrees;
+  int vacation;
+  cin >> degrees;
+  if (degrees>70)
+  {
+    vacation = 1;
+  }
+  else if (degrees<=70)
+  {
+    vacation = 0;
+  }
+}
