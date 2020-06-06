dd04209655d7428387b97b1d516c8c4c6039dd85
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-06-05 19:31:50 -0700

    Create StringtoCharArray.cpp

---
+++ Lesson-3/StringtoCharArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string hello = "hello";
+    char charArray[5];
+    charArray[0] = hello[0];
+    charArray[1] = hello[1];
+    charArray[2] = hello[2];
+    charArray[3] = hello[3];
+    charArray[4] = hello[4];
+    
+ }
