cfabb82b63da49c976c18e3bf8b78a3a4c5a5841
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-06-05 19:26:31 -0700

    Create AverageArray.cpp

---
+++ Lesson-3/AverageArray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4] = {1, 4, 7, 9};
+    int sum = arr[0] + arr[1] + arr[2] + arr[3];
+    double average = (double) sum / 4.0;
+    cout << average << endl;
+ }
