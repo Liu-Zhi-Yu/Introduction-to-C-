0726008b988eecfb272ed6ce3d1822535588f1f7
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-06-05 19:24:29 -0700

    Create CharArray.cpp

---
+++ Lesson-3/CharArray.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[4] = {'a', 'b', 'c', 'd'};
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+ }
