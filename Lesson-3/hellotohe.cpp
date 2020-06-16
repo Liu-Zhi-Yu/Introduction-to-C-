01fd945730382ea58ab475cf457daa800b625c1d
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-06-15 20:07:44 -0700

    Create hellotohe.cpp

---
+++ Lesson-3/hellotohe.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string hello = "hello";
+    hello.pop_back();
+    hello.pop_back();
+    hello.pop_back();
+    cout << hello << endl;
+}
